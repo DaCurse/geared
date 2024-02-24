2e20724060d267a38ba7f53377132b2988fccf25
Dacurse <dacurse0@gmail.com>
2024-02-24 17:34:00 +0200

    actually fix saves

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { loadState } from './state.ts'
 
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
+class ErrorBoundary extends React.Component<
+  PropsWithChildren,
+  { hasError: boolean }
+> {
+  constructor(props: PropsWithChildren) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
+
+  componentDidCatch() {
+    // You can also log the error to an error reporting service
+    setTimeout(() => {
+      localStorage.clear()
+      loadState().then(() => location.reload())
+    }, 1000)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h2>Sorry, your state is corrupted, resetting...</h2>
+    }
+
+    return this.props.children
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  )
-} catch {
-  // Simple save fixing mechanism
-  localStorage.clear()
-  loadState().then(() => location.reload())
-}
+  </ErrorBoundary>
+)
