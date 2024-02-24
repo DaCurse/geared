49532f65b735936a5361a27ef1b37b1f958bc85f
Dacurse <dacurse0@gmail.com>
2024-02-24 17:25:07 +0200

    fix broken saves by overwriting for now

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { loadState } from './state.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+try {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} catch {
+  // Simple save fixing mechanism
+  localStorage.clear()
+  loadState().then(() => location.reload())
+}
